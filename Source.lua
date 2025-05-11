ded011ff2392a4a2ccad101cca0e2eb89c8f5dc3
kiryxa <163583252+kirayxa2@users.noreply.github.com>
2025-05-11 15:55:54 +0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -77,7 +77,7 @@ function Neverlose_Main:PlaySound(SoundID)
     sound:Play()
 end
 
-local BuildInfo = "IDK"
+local BuildInfo = loadstring(game:HttpGet"https://pastebin.com/raw/t9LMPVt3")()
 
 local function MakeDraggable(topbarobject, object)
     local Dragging = nil
